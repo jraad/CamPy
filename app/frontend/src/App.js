60ab23eb111869c73651a104e0a56f83c82396ba
Jad Raad <jraad@Jads-MacBook-Pro.local>
2025-03-10 18:10:56 -0400

    Dockerized environments

---
+++ app/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CamPy NVR</h1>
+        <p>Network Video Recorder</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
